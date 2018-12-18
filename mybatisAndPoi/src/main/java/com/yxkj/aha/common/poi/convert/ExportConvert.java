0ff1e85da3eae5e6c754cf00f5faddba27549c40
zhz <1158809285@qq.com>
2018-12-18 17:07:35 +0800

    poi

---
+++ mybatisAndPoi/src/main/java/com/yxkj/aha/common/poi/convert/ExportConvert.java
@@ -0,0 +1,7 @@
+package com.yxkj.aha.common.poi.convert;
+
+
+public interface ExportConvert {
+
+    String handler(Object val);
+}
---
+++ mybatisAndPoi/src/test/java/com/yxkj/aha/AhaApplicationTests.java
@@ -0,0 +1,21 @@
+package com.yxkj.aha;
+
+import com.yxkj.aha.mapper.userMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AhaApplicationTests {
+
+	@Autowired
+	userMapper userMapper;
+	@Test
+	public void contextLoads() {
+		System.out.println(userMapper.selectOne());
+	}
+
+}
