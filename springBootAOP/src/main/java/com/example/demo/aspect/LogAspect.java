4c1834ca81a02e228cdbd7ca77110993f3cff97e
userTest <1158809285@qq.com>
2020-01-13 23:46:04 +0800

    更新aop代码

---
+++ springBootAOP/src/main/java/com/example/demo/aspect/LogAspect.java
@@ -0,0 +1,87 @@
+package com.example.demo.aspect;
+
+import com.example.demo.core.Log;
+import com.example.demo.pojo.SysLog;
+import com.example.demo.repository.SysLogRepository;
+import com.example.demo.utils.HttpContextUtils;
+import com.example.demo.utils.IPUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@Aspect
+@Component
+public class LogAspect {
+    @Autowired
+    private SysLogRepository sysLogDao;
+
+    @Pointcut("@annotation(com.example.demo.core.Log)")
+    public void pointcut() { }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint point) {
+        Object result = null;
+        long beginTime = System.currentTimeMillis();
+        try {
+            // 执行方法
+            result = point.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        // 执行时长(毫秒)
+        long time = System.currentTimeMillis() - beginTime;
+        // 保存日志
+        saveLog(point, time);
+        return result;
+    }
+
+    private void saveLog(ProceedingJoinPoint joinPoint, long time) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        SysLog sysLog = new SysLog();
+        Log logAnnotation = method.getAnnotation(Log.class);
+        if (logAnnotation != null) {
+            // 注解上的描述
+            sysLog.setOperation(logAnnotation.value());
+        }
+        // 请求的方法名
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = signature.getName();
+        sysLog.setMethod(className + "." + methodName + "()");
+        // 请求的方法参数值
+        Object[] args = joinPoint.getArgs();
+        // 请求的方法参数名称
+        LocalVariableTableParameterNameDiscoverer u = new LocalVariableTableParameterNameDiscoverer();
+        String[] paramNames = u.getParameterNames(method);
+        if (args != null && paramNames != null) {
+            String params = "";
+            for (int i = 0; i < args.length; i++) {
+                params += "  " + paramNames[i] + ": " + args[i];
+            }
+            sysLog.setParams(params);
+        }
+        // 获取request
+        HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
+        // 设置IP地址
+        sysLog.setIp(IPUtils.getIpAddr(request));
+        // 模拟一个用户名
+        sysLog.setUserName("kaleldo");
+        sysLog.setTime(new Date(time));
+        sysLog.setCreateTime(new Date());
+        // 保存系统日志
+        sysLogDao.save(sysLog);
+    }
+}
