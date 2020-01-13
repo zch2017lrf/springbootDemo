d42835d9ee1e3c55204e733a5f60d0b822f1152d
zch2017lrf <zch1158809285@163.com>
2020-01-13 14:09:04 +0800

    添加过滤器及拦截器

---
+++ springBootFilterAndInterceptor/src/main/java/com/example/demo/config/InterceptorConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo.config;
+
+import com.example.demo.interceptor.TimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    private TimeInterceptor timeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(timeInterceptor);
+    }
+}
---
+++ springBootFilterAndInterceptor/src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import com.example.demo.filter.TimeFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean timeFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        TimeFilter timeFilter = new TimeFilter();
+        filterRegistrationBean.setFilter(timeFilter);
+
+        List<String> urlList = new ArrayList<>();
+        urlList.add("/*");
+
+        filterRegistrationBean.setUrlPatterns(urlList);
+        return filterRegistrationBean;
+    }
+}
