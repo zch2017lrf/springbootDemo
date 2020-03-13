70d69d55ddc110ce491843105b2de86eb6a11698
kaleldo <zch1158809285@163.com>
2020-03-13 10:07:52 +0800

    简单整合SpringSecurity

---
+++ springBootSecurity/src/main/java/com/example/demo/config/BrowserSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin() // 表单方式
+                .and()
+                .authorizeRequests() // 授权配置
+                .anyRequest()  // 所有请求
+                .authenticated(); // 都需要认证
+    }
+}
