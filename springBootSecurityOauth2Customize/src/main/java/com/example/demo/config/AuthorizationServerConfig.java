51fec7856c00f4bbed947d124e8c67d90c638923
kaleldo <zch1158809285@163.com>
2020-03-14 09:26:33 +0800

    Oauth2自定义令牌

---
+++ springBootSecurityOauth2Customize/src/main/java/com/example/demo/config/AuthorizationServerConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+//@Order(1)
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ springBootSecurityOauth2Customize/src/main/java/com/example/demo/validate/smscode/SmsCode.java
@@ -0,0 +1,17 @@
+package com.example.demo.validate.smscode;
+
+public class SmsCode {
+    private String code;
+
+    public SmsCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
